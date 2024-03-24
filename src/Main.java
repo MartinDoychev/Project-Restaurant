fe40027248749e7444efc2ae996bdf3f67076ce9
yasen <yasena1234@abv.bg>
2024-03-24 23:23:20 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("JavaBite Bistro");
+        System.out.println("");
+
         Restaurant obj = new Restaurant("JavaBite", 4, 21000);
         Menu menu = new Menu();
         Revenue revenue = new Revenue();
         EmployeеList employeеList = new EmployeеList();
-        List<Table> tables = new ArrayList<>();
 
+        List<Table> tables = new ArrayList<>();
 
         Table seatsTable = new Table(null);
         int numberTable = seatsTable.Seats();
         seatsTable.Seats();
+
         menu.printMenu();
+
         List<Waiter> waiters = new ArrayList<>();
         for (Employee employee : employeеList.getEmployee()) {
             if (employee instanceof Waiter) {
                 waiters.add((Waiter) employee);
             }
         }
+
         if (!waiters.isEmpty()) {
             for (int i = 1; i <= numberTable; i++) {
                 if (i < 12) {
@@ -44,15 +52,14 @@ public class Main {
             System.out.printf("Table %d, Waiter: %s. \n", table.getId(), table.getWaiter().getName());
             table.takeOrderFromTable(menu, table.getWaiter());
             table.printBill();
-
-
-            for (Waiter waiter : waiters) {
-                System.out.printf("\nWaiter %s collected tips: %.2f \n", waiter.getName(), waiter.getTotalTip());
-
-            }
-
-            System.out.printf("\nTotal Revenue: %.2f%n", revenue.gerNetRevenue());
-            scanner.close();
+            revenue.addRevenue(table.gettotalBill());
         }
+
+        for (Waiter waiter : waiters) {
+            System.out.printf("\nWaiter %s collected tips: %.2f $.\n", waiter.getName(), waiter.getTotalTip());
+        }
+
+        System.out.printf("\nTotal Revenue: %.2f $.\n", revenue.gerNetRevenue());
+        scanner.close();
     }
 }
